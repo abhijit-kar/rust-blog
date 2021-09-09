d8e503d262e30721895c031b8d5dace6a514a41d
Abhijit Kar <reachme@abhijit-kar.com>
2021-09-09 20:18:19 +0530

    Made template parsing recoverable!

--- src/lib/watcher.rs
+++ src/lib/watcher.rs
@@ -10,24 +10,28 @@ pub fn watch_template(tera: Arc<RwLock<Tera>>) {
     tokio::spawn(async move {
         let (tx, rx) = channel();
 
-        let mut watcher: RecommendedWatcher =
-            Watcher::new(tx, Duration::from_millis(100)).expect("Failed to create watcher!");
+        let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(100))
+            .unwrap_or_else(|err| {
+                println!("Failed to create watcher!: {:?}", err);
+                ::std::process::exit(1);
+            });
 
         watcher
             .watch(
                 concat!(env!("CARGO_MANIFEST_DIR"), "/templates/"),
                 RecursiveMode::Recursive,
             )
-            .expect("Failed to watch Templates directory!");
+            .unwrap_or_else(|err| {
+                println!("Failed to watch!: {:?}", err);
+            });
 
         loop {
             match rx.recv() {
                 Ok(event) => {
                     if let DebouncedEvent::Write(_) = event {
-                        tera.write()
-                            .unwrap()
-                            .full_reload()
-                            .expect("Failed to Reload!")
+                        tera.write().unwrap().full_reload().unwrap_or_else(|err| {
+                            println!("Failed to reload!: {:?}", err);
+                        });
                     }
                 }
                 Err(e) => println!("Error while watching: {:?}", e),
